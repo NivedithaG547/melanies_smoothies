c25510cb5009676dc2401651193953fb66294d09
NivedithaG547 <niveditha.g@mrcooper.com>
2024-07-03 15:44:16 +0530

    Update streamlit_app.py
    Updated to display the nutritional information

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -54,4 +55,6 @@ if ingredient_list:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered! {name_on_order}!", icon="✅")
 
- 
+#New Section to display fruityvice nutrition information
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
